ce9fa764085ffe5b90ec41100b13f36b61eefb5b
michielfbr <michiel_brongers@hotmail.com>
2022-01-06 13:06:46 +0100

    fetches 3 new paintings everytime by adding pagination in state

--- store/art/actions.js
+++ store/art/actions.js
@@ -10,10 +10,10 @@ const paintingFetched = (data) => {
 };
 
 // Get paintings
-export function fetchPainting() {
+export function fetchPainting(pageNo) {
   return async function thunk(dispatch, getState) {
     try {
-      const res = await axios.get(`${apiUrl}&toppieces=true&imgonly=true`);
+      const res = await axios.get(`${apiUrl}&toppieces=true&imgonly=true&ps=3&p=${pageNo}`);
       dispatch(paintingFetched(res.data.artObjects));
     } catch (e) {
       console.log(e);
